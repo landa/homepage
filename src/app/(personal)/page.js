2b7079d9a810ab29b866a5b2a7895a3c3bb80462
Yafim <landa@alum.mit.edu>
2025-09-24 22:34:49 -0400

    Add app hosting emulator configuration, update dependencies, and create personal page layout

---
+++ src/app/(personal)/page.js
@@ -0,0 +1,38 @@
+import { Card, Text, Flex, Box, Avatar, Inset } from "@radix-ui/themes";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <Box>
+                    <Card>
+                        <Flex direction="row" gap="5">
+                            <Inset side="left">
+                                <Image width="200" height="200" src="/yafim.png" alt="Yafim Landa" />
+                            </Inset>
+                            <Flex direction="column" gap="4" maxWidth="300px">
+                                <Flex direction="row" gap="5">
+                                    <Flex direction="column" pt="2">
+                                        <Text weight="bold" size="6">
+                                            Yafim Landa
+                                        </Text>
+                                        <Text size="4" color="gray">
+                                            <a href="mailto:yafim@cloudstrat.com">
+                                                yafim@cloudstrat.com
+                                            </a>
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                                <Text size="4" color="gray">
+                                    Founder <a href="https://cloudstrat.com">@CloudStrat</a>, the best data processing software for small teams.
+                                </Text>
+                            </Flex>
+                        </Flex>
+                    </Card>
+                </Box>
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+        </div>
+    );
+}
