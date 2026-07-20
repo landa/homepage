3da969e25b7f4d49cae2726e377f9d6abe0d8fb9
Yafim <landa@alum.mit.edu>
2026-07-20 04:15:36 -0400

    Add new icons and web manifest for improved branding and user experience

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,16 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Yafim Landa",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     title: "Yafim Landa",
     images: [
